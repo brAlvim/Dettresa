ccf825bd1daf6c7da3f2aeadf13eec9b368e708e
Paulo César Teixeira <pauloc.2505@gmail.com>
2015-02-22 15:46:43 -0300

    Update

---
+++ index.php
@@ -0,0 +1,152 @@
+<html>
+<head>
+	<title>Dettresa</title>
+	<meta name="author" content="LPC // Marketing Digital // 041 9800-0666">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<nav>
+		<ul>
+			<li><a href="" title="">Home</a></li>
+			<li><a href="" title="">Sobre a empresa</a></li>
+			<li><a href="" title="">Coaching</a></li>
+			<li><a href="" title="">Serviços</a></li>
+			<li><a href="" title="">Formação</a></li>
+			<li><a href="" title="">Contato</a></li>
+			<li style="float: right"><a href="" title="">Acessar blog</a></li>
+
+		</ul>
+	</nav>
+	<!--
+	HOME
+	-->
+	<div id="home">
+		<img class="logo" src="images/logo-home.png" alt="">
+		<div class="subtitle">
+			Tenha uma carreira de sucesso<br>com a <span>Dettresa</span>.<br><br>
+			<a href="#" title="">Clique aqui</a> e descubra<br>como podemos lhe ajudar.
+		</div>
+	</div>
+	<!--
+	DIVISOR
+	-->
+	<div class="divisor1"></div>
+	<!--
+	SOBRE A EMPRESA	
+	-->
+	<div id="sobre">
+
+	</div>
+
+		
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+</body>
+</html>
